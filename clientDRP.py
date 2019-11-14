9af188fc98412128c22e8e3eb8fa9b5655e002f8
jkeay <jkeay@sfu.ca>
2019-11-13 17:08:20 -0800

    Moved code into functions to clean up everything

--- clientDRP.py
+++ clientDRP.py
@@ -1,28 +1,24 @@
 from socket import *
 from drpPacket import *
 
-bufferSize = 10
-serverName = '127.0.0.1'
-serverPort = 12000
+def main():
+	orderBuffer = []
+	recievedBuffer = []
+	clientSocket = setupConnection('127.0.0.1', 12000, 10)
 
-orderBuffer = []
-recievedBuffer = []
+	# while True:
+	message, clientAddress = clientSocket.recvfrom(2048)
+	packet = parseDrpPacket(message.decode())
+	data = packet.getData()
+	print data
+	clientSocket.close()
 
-def setupConnection():
+def setupConnection(serverName, serverPort, bufferSize):
 	clientSocket = socket(AF_INET, SOCK_DGRAM)
 	packet = createConnectionPacket(bufferSize)
 	message = packet.toString()
 	clientSocket.sendto(message.encode(), (serverName, serverPort))
 	return clientSocket
 
-clientSocket = setupConnection()
-
-# while True:
-message, clientAddress = clientSocket.recvfrom(2048)
-packet = parseDrpPacket(message.decode())
-data = packet.getData()
-print data
-clientSocket.close()
-
-
-    # clientSocket.sendto(modifiedMessage.encode(), clientAddress)
+if __name__ == "__main__":
+	main()
--- serverDRP.py
+++ serverDRP.py
@@ -1,27 +1,33 @@
 from socket import *
 from drpPacket import *
 
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
-data = "STUFF TO SEND"
+def main():
+	serverPort = 12000
+	serverSocket = socket(AF_INET, SOCK_DGRAM)
+	serverSocket.bind(('', serverPort))
+	data = "STUFF TO SEND"
 
-message, clientAddress = serverSocket.recvfrom(2048)
-packet = parseDrpPacket(message.decode())
-print "Type: " + str(packet.getHeaderValue("type"))
-print "BufferSize: " + str(packet.getHeaderValue("bufferSize"))
+	print "Server Waiting For Request..."
+	message, clientAddress = serverSocket.recvfrom(2048)
+	packet = parseDrpPacket(message.decode())
+	print "Type: " + str(packet.getHeaderValue("type"))
+	print "BufferSize: " + str(packet.getHeaderValue("bufferSize"))
 
-packetToSend = createDataPacket(ReliabilityType.PEC, 1, data)
-serverSocket.sendto(packetToSend.encode(), clientAddress)
-serverSocket.close()
+	packetToSend = createDataPacket(ReliabilityType.PEC, 1, data)
+	serverSocket.sendto(packetToSend.encode(), clientAddress)
+	serverSocket.close()
 
-"""
-while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
-"""
+	"""
+	while True:
+	    message, clientAddress = serverSocket.recvfrom(2048)
+	    modifiedMessage = message.decode().upper()
+	    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+	"""
 
-# packet = createDataPacket(0, "chunk")
-# packet = loadRdpPacket('{"header":{"retransmission":true},"body":"DATA TO SEND"}');
-# print(packet.toString())
+	# packet = createDataPacket(0, "chunk")
+	# packet = loadRdpPacket('{"header":{"retransmission":true},"body":"DATA TO SEND"}');
+	# print(packet.toString())
+
+if __name__ == "__main__":
+	main()
+	
